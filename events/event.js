6e3388e852ac682db659ae077522973805a1ff49
Thiago Roberto <thiago.nemo4code@gmail.com>
2019-12-25 10:18:19 -0300

    trabalhando com biblioteca nativa de eventos

--- events/event.js
+++ events/event.js
@@ -1,11 +1,19 @@
-const EventEmmiter = require("events");
+const { EventEmitter } = require("events");
 
-class Evento extends EventEmmiter {}
+class Evento extends EventEmitter {}
 
 const meuEvento = new Evento();
 
+// subscriber - assinante
 meuEvento.on("seguranca", (x, y) => {
   console.log(`Executando o evento 'seguranca': ${x} ${y}`);
 });
 
+// publisher - emissor
 meuEvento.emit("seguranca", "userAdmin", "Alterou Salário");
+
+meuEvento.on("encerrar", dados => {
+  console.log("Assinante: " + dados);
+});
+
+meuEvento.emit("encerrar", "encerrando a execução da importação dos dados!");
