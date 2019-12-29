65ab8bf829f8dedf793cc8e2f5c07259f99e3239
Thiago Roberto <thiago.nemo4code@gmail.com>
2019-12-29 09:23:47 -0300

    trabalhando com módulos externos e os exportando. Utilizando REPL para testar

---
+++ modules/modulo-01.js
@@ -0,0 +1,11 @@
+console.log("executando modulo-01.js");
+
+module.exports.oculta = () => {
+  console.log("executando função oculta");
+};
+
+module.exports.executa = () => {
+  console.log("executando a função executa");
+};
+
+module.exports.welcome = "bem vindo ao módulo modulo-01.js";
